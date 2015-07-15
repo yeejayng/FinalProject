84979eee51e038a3a40f959c4335c926dbafd30c
Cluster 5 <cluster5@dhcp-v006-134.mobile.uci.edu>
2015-07-15 09:53:39 -0700

    Copy of what I had before the switch
    Labels in the correct position, but the keys are still in random
    positions.
    //TODO Assign each key a value and be able to change the color
    appropriately

--- FinalProject/ViewController.swift
+++ FinalProject/ViewController.swift
@@ -15,10 +15,13 @@ class ViewController: UIViewController
     var analyzer = AKAudioAnalyzer()
     var updateAnalysis = AKEvent()
     var analysisSequence = AKSequence()
+    var noteFrequencies = [16.35,17.32,18.35,19.45,20.6,21.83,23.12,24.5,25.96,27.5,29.14,30.87]
+    var noteNamesWithFlats  = ["C", "D♭","D","E♭","E","F","G♭","G","A♭","A","B♭","B"]
     
     @IBOutlet weak var frequencyLabel: UILabel!
-    @IBOutlet weak var noteNameLabel: UILabel!//
-    
+    @IBOutlet weak var noteNameLabel: UILabel!
+    @IBOutlet weak var allNotesDisplay: UILabel!
+
     
     override func viewDidLoad()
     {
@@ -29,6 +32,7 @@ class ViewController: UIViewController
         AKOrchestra.addInstrument(mic)
         analyzer = AKAudioAnalyzer(audioSource: mic.auxilliaryOutput)
         AKOrchestra.addInstrument(analyzer)
+        println("Loaded")
         
     }
     
@@ -37,20 +41,57 @@ class ViewController: UIViewController
         super.viewDidAppear(true)
         analyzer.start()
         mic.start()
+        allNotesDisplay.text = ""
         
         updateAnalysis = AKEvent(block: {
             self.updateUI()
             self.analysisSequence.addEvent(self.updateAnalysis, afterDuration: 0.1)
+            println("Analysis Updated")
         })
+        analysisSequence.addEvent(updateAnalysis)
+        analysisSequence.play()
+        println("did appear")
     }
     
     func updateUI()
     {
+        println("UI updated")
         println(analyzer.trackedFrequency.value)
-        if analyzer.trackedAmplitude.value > 0.1
+
+        var frq = abs(Double(analyzer.trackedFrequency.value))
+        while frq > noteFrequencies.last
+        {
+            println("greater")
+            frq = frq / 2.0
+        }
+        while frq < noteFrequencies.first && frq != 0
+        {
+            println("lower")
+            frq = frq * 2
+        }
+        println(frq)
+        
+        var minDistance = Double(10000);
+        var index = 0;
+        for var count = 0; count < noteFrequencies.count; count++
+        {
+//            println(index)
+            var distance = abs(noteFrequencies[count] - frq)
+            if distance < minDistance
+            {
+                index = count
+                minDistance = distance
+            }
+        }
+        
+        if analyzer.trackedAmplitude.value > 0.001
         {
             frequencyLabel.text="\(analyzer.trackedFrequency.value)"
+            noteNameLabel.text="\(noteNamesWithFlats[index])"
+            allNotesDisplay.text = "\(allNotesDisplay.text! + noteNamesWithFlats[index])"
         }
+
+        
     }
     
 
---
+++ README.md
@@ -0,0 +1,4 @@
+# group_02
+This app combines the two ideas of pitch detection and soothing sounds for people with PTSD or stress problems. 
+There will be around 5 or 4 soothing sounds, and the range of pitches will also be divided into 4. Each range of pitches will correlate to a soothing sound with a similar range of frequencies.
+If you play or sing a pitch in that range of frequencies, the app will understand that and play the corresponding soothing sound for that range of frequencies.
